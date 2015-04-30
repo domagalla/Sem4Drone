6df5d54142f78b979004d2e3c0f0473eeed7c5ea
Oliver <oliverkaup@gmx.de>
2015-04-30 13:56:58 +0200

    Ollis Änderung

--- src/ardrone/DroneActorPositionAdapter.java
+++ src/ardrone/DroneActorPositionAdapter.java
@@ -5,10 +5,23 @@
  */
 package ardrone;
 
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  *
  * @author Oliver
  */
-public class DroneActorPositionAdapter {
+public class DroneActorPositionAdapter implements Observer{
+
+    @Override
+    public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void notifyDroneControl(double x, double y, double z){
+        
+    }
+    
     
 }
