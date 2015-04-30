9bfe3965eaead42cf146c520c5b4f3693808253e
Oliver <oliverkaup@gmx.de>
2015-04-30 14:26:56 +0200

    Ollis Änderung

--- src/ardrone/ScenarioASim.java
+++ src/ardrone/ScenarioASim.java
@@ -5,10 +5,43 @@
  */
 package ardrone;
 
+import java.util.ArrayList;
+import java.util.Observable;
+
 /**
  *
  * @author Oliver
  */
-public class ScenarioASim {
-    
+public class ScenarioASim implements World{
+        ArrayList <Actor> actorList = new ArrayList<Actor>();
+	
+	public Actor getActor(int index){
+		return actorList.get(index);
+	}
+	public void createActor(int index){
+		Actor a = new Actor();
+                actorList.add(a);
+	}
+	public void removeActor(Actor a){
+		if(actorList.remove(a)){
+                    System.out.println("Actor wurde entfernt");
+                } else {
+                    System.out.println("Actor nicht vorhanden");
+                }
+	}
+	public int getActorsCount(){
+		return actorList.size();
+	}
+	public void setChanged(){
+		
+	}
+        
+	public void notifyObservers(Object arg){
+		
+	}
+
+        @Override
+        public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
