750fb8486ababa1454638d493e11324b7ef79843
Oliver <oliverkaup@gmx.de>
2015-04-30 16:07:47 +0200

    Ollis Änderung

--- src/ardrone/World.java
+++ src/ardrone/World.java
@@ -12,14 +12,35 @@ import java.util.Observer;
  *
  * @author Oliver
  */
-public interface World extends Observer{
+public class World implements WorldControl{
     
         ArrayList <Actor> actorList = new ArrayList<Actor>();
 	
-	public Actor getActor(int index);
-	public void createActor();
-	public void removeActor(Actor a);
-	public int getActorsCount();
-	public void setChanged();
-	public void notifyObservers(Object arg);
+	public Actor getActor(int index){
+		return actorList.get(index);
+	}
+	public void createActor(){
+		Actor a = new Actor();
+                actorList.add(a);
+	}
+	public void removeActor(Actor a){
+		if(actorList.remove(a)){
+                    System.out.println("Actor wurde entfernt");
+                } else {
+                    System.out.println("Actor nicht vorhanden");
+                }
+	}
+	public int getActorsCount(){
+		return actorList.size();
+	}
+	public void setChanged(){
+		
+	}
+        
+	public void notifyObservers(Object arg){
+		getActor(0).getPosX();
+                getActor(0).getPosY();
+                getActor(0).getPosZ();
+	}
+
 }
