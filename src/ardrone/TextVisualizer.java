6d1ac7adae77adaa3dedec3c893e0e9b3b3a7753
Oliver <oliverkaup@gmx.de>
2015-05-01 14:38:42 +0200

    Interfaces

--- src/ardrone/TextVisualizer.java
+++ src/ardrone/TextVisualizer.java
@@ -14,15 +14,18 @@ import java.util.Observer;
  */
 public class TextVisualizer implements Observer{
     
+    //Der Textvisualizer sollte die GUI erstellen und in der Methode visualize() die GUI mit den Daten aus pos[] füttern
     
     @Override
     public void update(Observable o, Object arg) {
+        //nur ausführen wenn nicht nur die Drohne für das Model übergeben wird
         if(!arg.getClass().getName().equals("ardrone.Actor")){
             visualize((double[])arg);
         }
     }
     
     public void visualize(double[] pos){
+        //pos beinhaltet Position in x, y und z und die Rotation um die Z-Achse
 	System.out.println("Drone bewegt sich auf " + pos[0] +", "+ pos[1] +", "+ pos[2]);
         System.out.println("Drohne ist um "+ pos[3] +" Grad rotiert");
     }
