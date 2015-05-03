7194775d65d502704f70d0fb8286341815d581d4
Admin <Admin@Admin-PC.Speedport_W_724V_09011602_00_015>
2015-05-03 11:49:06 +0200

    neue Gui2

--- src/ardrone/GUI.java
+++ src/ardrone/GUI.java
@@ -61,20 +61,18 @@ public class GUI extends JFrame implements Runnable {
  
      this.setTitle("ARDon Stats");
      this.setSize(1024,768);
- 
-            this.setVisible(true);
-   
+     this.setVisible(true);
+     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
      }
     
     @Override
     public void run() {
-    
          createFrame();
             }
     
     @Override
     public void paint(Graphics g){
-    g.clearRect(0, 0, getWidth(), getHeight());
+         g.clearRect(0, 0, getWidth(), getHeight());
         g.fillOval((int)Math.round(x)+100,(int)Math.round(y)+100, 10, 10);
         g.drawString("ARDrone", (int)Math.round(x)+115,(int)Math.round(y)+110);
         g.setColor(Color.black);
