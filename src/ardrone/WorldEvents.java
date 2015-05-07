e17036652d329b9edca47b49602e9fa02c30953f
Oliver <oliverkaup@gmx.de>
2015-05-07 14:12:53 +0200

    Auf neue Schnittstellendefinition angepasst

--- src/ardrone/WorldEvents.java
+++ src/ardrone/WorldEvents.java
@@ -5,10 +5,15 @@
  */
 package ardrone;
 
+import java.util.Observer;
+
 /**
  *
  * @author Oliver Kaup
  */
 public interface WorldEvents {
     
+    public void register(Observer o);
+    public void unregister(Observer o);
+    public void notifyObserver();
 }
