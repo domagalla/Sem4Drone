4ac3e3510ef927ce70c4358c9a384074891d23a0
Oliver <oliverkaup@gmx.de>
2015-05-07 13:38:11 +0200

    Auf neue Schnittstellendefinition angepasst

--- src/ardrone/Actor.java
+++ src/ardrone/Actor.java
@@ -1,72 +1,26 @@
+package ardrone;
+
+
+import java.util.HashMap;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ardrone;
-
-import java.util.HashMap;
 
 /**
  *
- * @author Marvin Voß
+ * @author Oliver
  */
-public class Actor {
+public interface Actor {
     
-        //Hashmap zur Identifizierung des Actors
-        private HashMap<String, Object> attributeList = new HashMap<String, Object>();
-	private double PosX, PosY, PosZ, RotX, RotY, RotZ;
-	
-	public double getPosX(){
-		return PosX;
-	}
-	public double getPosY(){
-		return PosY;
-	}
-	public double getPosZ(){
-		return PosZ;
-	}
-	public double getRotX(){
-		return RotX;
-	}
-	public double getRotY(){
-		return RotY;
-	}
-	public double getRotZ(){
-		return RotZ;
-	}
-	public void setRot(double x, double y, double z){
-		RotX = x;
-		RotY = y;
-		RotZ = z;
-	}
-	public void setPos(double x, double y, double z){
-		PosX = x;
-		PosY = y;
-		PosZ = z;
-	}
-        //Neuse Attribut anlegen
-	public void createAttribute(String name, Object value){
-		attributeList.put(name, value);
-	}
-        //Vorhandenes Attribut ändern
-	public void setAttribute(String name, Object value){
-		attributeList.replace(name, value);
-	}
-        //Gibt den Wert des gesuchten Attributs zurück
-	public Object getAttribute(String name){
-		return attributeList.get(name);
-	}
-        //Löscht das gesuchte Attribut
-	public void deleteAttribut(String name){
-		attributeList.remove(name);
-	}
-        //Gibt die Hashmap mit allen Attributen zurück
-	public HashMap<String, Object> getAllAttributes(){
-		return attributeList;
-	}
-        //Setzt alle Attribute durch mitgabe einer HashMap (Bsp. zum Laden aus einer Datei)
-        public void setAllAttributes(HashMap<String, Object> loadFile){
-                attributeList = loadFile;
-        }
+    public double[] getPosition();
+    public double[] getRotation();
+    public void setRotation(double x, double y, double z);
+    public void setPosition(double x, double y, double z);
+    public void createAttribute(String name, Object value);
+    public Object getAttribute(String name);
+    public void deleteAttribute(String name);
+    public HashMap<String, Object> getAllAttributes();
 }
